d6d78560b17cb9a7d99693cc3246911f2157c841
Lauren Fay <laurenfay.rosenberg@mail.utoronto.ca>
2019-04-05 16:38:46 -0400

    Interpreter

--- A4.hs
+++ A4.hs
@@ -89,6 +89,80 @@ chainl2 arg op = do
         y <- arg
         return (f x y)
 
-
 mainInterp :: Expr -> Either Error Value
-mainInterp = error "TODO"
+mainInterp = interp Map.empty
+
+intOrDie :: Value -> Either Error Integer
+intOrDie (VN i) = pure i
+intOrDie _ = Left TypeError
+
+boolOrDie :: Value -> Either Error Bool
+boolOrDie (VB b) = pure b
+boolOrDie _ = Left TypeError
+
+interp :: Map String Value -> Expr -> Either Error Value
+
+interp _ (Num i) = pure (VN i)
+interp _ (Bln b) = pure (VB b)
+
+interp env (Var v) = case Map.lookup v env of
+    Just a -> pure a
+    Nothing -> Left VarNotFound
+
+interp env (Prim2 Eq e1 e2) = do
+    a <- interp env e1
+    b <- interp env e2
+    case a of
+        VB _ -> case b of 
+                  VB _ -> let i = boolOrDie a;
+                              j = boolOrDie b;
+                         in pure (VB (i == j))
+                  VN _ -> Left TypeError
+        VN _ -> case b of
+                  VB _ -> Left TypeError
+                  VN _ -> let i = intOrDie a;
+                              j = intOrDie b;
+                         in pure (VB (i == j))
+
+interp env (Prim2 op e1 e2) = do
+  a <- interp env e1
+  i <- intOrDie a
+  b <- interp env e2
+  j <- intOrDie b
+  case op of
+    Plus -> pure(VN (i + j))
+    Mul -> pure(VN (i * j))
+    Minus -> pure(VN (i - j))
+    Div -> case j == 0 of 
+                False -> pure(VN (i `div` j))
+                True -> Left DivByZero
+    Mod -> pure(VN (mod i j))
+    Lt -> pure(VB (i < j))
+
+interp env (Cond test eThen eElse) = do
+    a <- interp env test
+    case a of
+        VB True -> interp env eThen
+        VB False -> interp env eElse
+        _ -> Left TypeError
+
+interp env (Let equations inClause) = do
+    extendedEqs <- extend env equations
+    interp extendedEqs inClause
+  where
+    extend env [] = pure env
+    extend env ((v, rhs) : equations) = do
+        a <- interp env rhs
+        let extendedEqs = Map.insert v a env
+        extend extendedEqs equations
+
+interp env (Lambda v body) = pure (VClosure env v body)
+
+interp env (App f e) = do 
+    c <- interp env f
+    case c of 
+      VClosure fEnv v body -> do
+        eVal <- interp env e
+        let bEnv = Map.insert v eVal fEnv
+        interp bEnv body
+      _ -> Left TypeError
